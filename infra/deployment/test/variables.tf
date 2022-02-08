0f96bc760722b819caa9981a14144c31b84cb413
JPaiv <juho.paivarinta@outlook.com>
2022-02-08 11:58:09 +0000

    Remove unused variables.

--- infra/deployment/test/variables.tf
+++ infra/deployment/test/variables.tf
@@ -4,14 +4,7 @@ variable "environment" {
   default     = "test"
 }
 
-variable "default_region" {
-  type        = string
-  description = "The AWS region things are created in"
-  default     = "eu-west-1"
-}
-
 variable "image_tag" {
   type        = string
   description = "Docker image to run in the ECS cluster"
-}
 }
