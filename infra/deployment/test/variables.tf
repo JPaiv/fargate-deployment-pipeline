07103b1e39a8553529dd20c124a26fa9136b662c
JPaiv <juho.paivarinta@outlook.com>
2022-02-08 11:56:00 +0000

    Remove unused variables.

--- infra/deployment/test/variables.tf
+++ infra/deployment/test/variables.tf
@@ -10,48 +10,8 @@ variable "default_region" {
   default     = "eu-west-1"
 }
 
-variable "ecs_task_execution_role_name" {
-  type        = string
-  description = "ECS task execution role name"
-  default     = "EcsTaskExecutionRole"
-}
-
-variable "ecs_auto_scale_role_name" {
-  type        = string
-  description = "ECS auto scale role Name"
-  default     = "EcsAutoScaleRole"
-}
-
 variable "image_tag" {
   type        = string
   description = "Docker image to run in the ECS cluster"
 }
-
-variable "app_port" {
-  type        = number
-  description = "Port exposed by the docker image to redirect traffic to"
-  default     = 5000
-}
-
-variable "container_count" {
-  type        = number
-  description = "Number of docker containers to run"
-  default     = 3
-}
-
-variable "health_check_path" {
-  type    = string
-  default = "/"
-}
-
-variable "fargate_cpu" {
-  type        = string
-  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
-  default     = "1024"
-}
-
-variable "fargate_memory" {
-  type        = string
-  description = "Fargate instance memory to provision (in MiB)"
-  default     = "2048"
 }
