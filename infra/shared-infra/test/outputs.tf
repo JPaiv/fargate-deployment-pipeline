4c344d072dcee21bfb4952231a3c1795bc58d989
JPaiv <juho.paivarinta@outlook.com>
2022-02-08 07:27:16 +0000

    Add depdendson.

--- infra/shared-infra/test/outputs.tf
+++ infra/shared-infra/test/outputs.tf
@@ -1,13 +1,16 @@
 output "main_vpc_id" {
   value = module.vpc.output.main_vpc_id
+  depends_on = [module.vpc]
 }
 
 
 output "private_subnet_ids" {
   value = module.vpc.output.private_subnet_ids
+  depends_on = [module.vpc]
 }
 
 
 output "public_subnet_ids" {
   value = module.vpc.output.public_subnet_ids
+  depends_on = [module.vpc]
 }
