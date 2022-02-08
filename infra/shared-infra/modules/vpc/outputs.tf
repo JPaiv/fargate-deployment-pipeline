8981133729bd8b15d3e11bcbc7721a11696c6030
JPaiv <juho.paivarinta@outlook.com>
2022-02-08 07:17:13 +0000

    Get as list.

--- infra/shared-infra/modules/vpc/outputs.tf
+++ infra/shared-infra/modules/vpc/outputs.tf
@@ -3,9 +3,9 @@ output "main_vpc_id" {
 }
 
 output "private_subnet_ids" {
-  value = "${aws_subnet.privatec.*.id}"
+  value = ["${aws_subnet.privatec.*.id}"]
 }
 
 output "public_subnet_ids" {
-  value = "${aws_subnet.public.*.id}"
+  value = ["${aws_subnet.public.*.id}"]
 }
