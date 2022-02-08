114cd8b7417d163d1c79eba3542ca04f04a346cc
JPaiv <juho.paivarinta@outlook.com>
2022-02-08 11:35:50 +0000

    Fix intentation.

--- infra/deployment/modules/application/iam.tf
+++ infra/deployment/modules/application/iam.tf
@@ -51,24 +51,23 @@ EOF
 resource "aws_iam_role_policy_attachment" "ecs-task-role-policy-attachment" {
   role       = aws_iam_role.ecs_task_role.name
   policy_arn = aws_iam_policy.dynamodb.arn
-}
   name = "${var.name}-ecsTaskExecutionRole"
  
   assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": "sts:AssumeRole",
-     "Principal": {
-       "Service": "ecs-tasks.amazonaws.com"
-     },
-     "Effect": "Allow",
-     "Sid": ""
-   }
- ]
-}
-EOF
+    {
+     "Version": "2012-10-17",
+     "Statement": [
+       {
+         "Action": "sts:AssumeRole",
+         "Principal": {
+           "Service": "ecs-tasks.amazonaws.com"
+         },
+         "Effect": "Allow",
+         "Sid": ""
+       }
+     ]
+    }
+    EOF
 }
  
 resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
