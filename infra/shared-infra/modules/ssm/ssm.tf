a590c6f154f95f71f012da606991496cd6bcf844
JPaiv <juho.paivarinta@outlook.com>
2022-02-13 16:24:25 +0000

    Add overwite rules.

--- infra/shared-infra/modules/ssm/ssm.tf
+++ infra/shared-infra/modules/ssm/ssm.tf
@@ -7,11 +7,13 @@ resource "random_password" "aurora_master_password" {
 resource "aws_ssm_parameter" "ssm_aurora_master_username" {
   name  = "${var.environment}-aurora_master_username"
   type  = "SecureString"
+  overwrite = true
   value = var.aurora_master_username
 }
 
 resource "aws_ssm_parameter" "ssm_aurora_master_password" {
   name  = "${var.environment}-aurora_master_password"
   type  = "SecureString"
+  overwrite = true
   value = random_password.aurora_master_password.result
 }
