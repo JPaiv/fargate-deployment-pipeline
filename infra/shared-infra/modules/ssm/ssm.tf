5bd9159dbd10a0df1e8192f0e5e276bcc0a6d2cd
JPaiv <juho.paivarinta@outlook.com>
2022-02-13 14:18:09 +0000

    Fix username variable.

--- infra/shared-infra/modules/ssm/ssm.tf
+++ infra/shared-infra/modules/ssm/ssm.tf
@@ -4,14 +4,14 @@ resource "random_password" "aurora_cluster_master_password" {
   override_special = "!#$%^*()-=+_?{}|"
 }
 
-resource "aws_ssm_parameter" "ssm_aurora_cluster_master_password" {
-  name  = "${var.environment}-aurora_cluster_master_password"
-  type  = "SecureString"
-  value = random_password.aurora_cluster_master_password.result
-}
-
 resource "aws_ssm_parameter" "ssm_aurora_cluster_master_username" {
   name  = "${var.environment}-aurora_cluster_master_username"
   type  = "SecureString"
   value = var.aurora_master_username
+}
+
+resource "aws_ssm_parameter" "ssm_aurora_cluster_master_password" {
+  name  = "${var.environment}-aurora_cluster_master_password"
+  type  = "SecureString"
+  value = random_password.aurora_cluster_master_password.result
 }
